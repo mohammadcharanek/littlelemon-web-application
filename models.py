03b36d90805202b81ac8da1876c816797e714e13
MOHAMMAD Ali CHARANEK <51740757+mohammadcharanek@users.noreply.github.com>
2024-04-07 11:49:47 +0300

    Add files via upload

--- models.py
+++ models.py
@@ -1,10 +1,3 @@
 from django.db import models
 
-class Booking(models.Model):
-    # Your Booking model fields here
-    customer_name = models.CharField(max_length=100)
-    booking_date = models.DateField()
-    # Add more fields as needed
-
-    def __str__(self):
-        return self.customer_name
+# Create your models here.
--- urls.py
+++ urls.py
@@ -1,9 +1,22 @@
+"""
+URL configuration for littlelemon project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
 from django.urls import path
-from . import views
 
 urlpatterns = [
-    path('api/bookings/', views.booking_list),
-    path('api/bookings/<int:pk>/', views.booking_detail),
-    path('api/registration/', views.registration),
-    # Add more API paths as needed
+    path('admin/', admin.site.urls),
 ]
