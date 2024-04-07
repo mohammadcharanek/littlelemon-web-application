e2e707e6e1822444e5439993bc200551f8fdc9bd
MOHAMMAD Ali CHARANEK <51740757+mohammadcharanek@users.noreply.github.com>
2024-04-07 12:50:41 +0300

    Update views.py

--- views.py
+++ views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+# views.py
+
+from rest_framework import viewsets
+from .models import MenuItem
+from .serializers import MenuItemSerializer
+
+class MenuItemViewSet(viewsets.ModelViewSet):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
